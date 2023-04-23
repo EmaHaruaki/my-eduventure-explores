e1eb5cbd9a4d6b43071c9b0dbc964b1afa406ca4
EmaHaruaki <hase.yrsk@gmail.com>
2023-04-23 23:43:05 +0800

    update:add ga

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,8 +36,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '@/plugins/gtag',
   ],
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
